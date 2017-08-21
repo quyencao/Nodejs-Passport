80ff14d0b39f9ab579a36f9a3883d699b9023e28
quyencao <boy20195@gmail.com>
2017-08-21 20:43:45 +0700

    Register user

--- db.js
+++ db.js
@@ -8,4 +8,9 @@ var connection = mysql.createConnection({
 
 connection.connect()
 
+// connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+//     if (error) throw error;
+//     console.log('The solution is: ', results[0].solution);
+// });
+
 module.exports = connection;
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Registration' });
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Registration' });
 });
 
 router.post('/register', function (req, res, next) {
-   res.json(req.body);
+   const db = require('./../db');
+
+   const username = req.body.username;
+   const email = req.body.email;
+   const password = req.body.password;
+
+   db.query('INSERT INTO users(username, email, password) VALUES(?,?,?)',
+        [username, email, password], function (error, results, fields) {
+           if(error) throw error;
+
+           res.render('register', { title: 'Registration Complete' });
+       });
 });
 
 module.exports = router;
